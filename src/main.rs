630560d005e1b3076a85c8f7838b5c4335864da8
john <doneth7@gmail.com>
2019-10-23 11:45:20 -0400

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-
 use indicatif::{ProgressBar, ProgressStyle};
 use reqwest::Url;
 
@@ -9,27 +8,23 @@ use tokio::prelude::*;
 
 #[tokio::main]
 async fn main() {
-
     match std::env::args().nth(1) {
         Some(ref url) => {
-
             let url: Url = Url::from_str(url).unwrap();
 
             let file_name = url.path_segments().unwrap().last().unwrap();
-            
+
             let mut file = File::create(file_name).await.unwrap();
-            
-            let mut response = reqwest::get(url.clone())
-                .await.unwrap();
+
+            let mut response = reqwest::get(url.clone()).await.unwrap();
 
             let pb = ProgressBar::new(0);
 
             pb.set_message(&format!("Fetching {}", url));
-            
+
             pb.set_style(ProgressStyle::default_bar()
                 .template("{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] {bytes}/{total_bytes} ({bytes_per_sec}) (ETA {eta})")
                 .progress_chars("#>-"));
-
 
             if let Some(length) = response.content_length() {
                 pb.set_length(length);
@@ -42,12 +37,7 @@ async fn main() {
             }
 
             pb.finish();
-
         }
-        None => {
-            println!("Please supply a URL")
-        }
+        None => println!("Please supply a URL"),
     }
-
-    
 }
